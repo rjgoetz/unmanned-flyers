60154608b4eb789200e7f405e3959b6f61971ea6
rjgoetz <rj@rjgoetz.com>
2015-12-06 21:14:40 -0600

    owl-carousel; learn-to-fly.scss

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+	'use strict';
+	$('.owl-carousel').owlCarousel({
+	    autoplay:true,
+	    autoplayHoverPause:true,
+	    loop:true,
+	    items: 5,
+	    autoWidth: true
+	});
+});
