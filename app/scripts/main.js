ab5057b63922ee781d7c2faed41b420002321e3b
rjgoetz <rj@rjgoetz.com>
2015-12-06 23:01:37 -0600

    updated carousel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 	'use strict';
 	$('.owl-carousel').owlCarousel({
-	    // autoplay:true,
-	    // autoplayHoverPause:true,
-	    // loop:true,
-	    // items: 3,
-	    // autoWidth: true
+	    autoplay:true,
+	    autoplayHoverPause:true,
+	    loop:true,
+	    items: 3,
+	    autoWidth: true
 	});
 });
