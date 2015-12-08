25913e3d8d36e1d89a8169e81d553750a37e2dcb
RJ Goetz <rj@rjgoetz.com>
2015-12-08 14:31:26 -0600

    added google maps; styled maps page; need to add right image

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,16 @@
 "use strict";
 /*global $*/
 
+function initialize() {
+	var mapCanvas = document.getElementById('map');
+	var mapOptions = {
+		center: new google.maps.LatLng(44.9303324,-93.3054337),
+		zoom: 12,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	var map = new google.maps.Map(mapCanvas, mapOptions);
+};
+
 $(document).ready(function(){
 	$(".owl-carousel").owlCarousel({
 		autoplay: true,
@@ -9,4 +19,6 @@ $(document).ready(function(){
 		items: 3,
 		autoWidth: true
 	});
+
+	google.maps.event.addDomListener(window, 'load', initialize);
 });
